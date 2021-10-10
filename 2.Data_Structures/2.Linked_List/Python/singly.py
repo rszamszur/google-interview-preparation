be96a3c093503ae280c0c235d3d5182eb221f840
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-10-10 21:36:39 +0200

    Implement singly linked list with tests

---
+++ 2.Data_Structures/2.Linked_List/Python/singly.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python
+
+
+class Node(object):
+    """Node class implementation for linked list."""
+    
+    def __init__(self, value):
+        """Initialize Node class object instance."""
+        self.value = value
+        self.next = None
+    
+    def __str__(self):
+        """Node class __str__ method."""
+        return str(self.value)
+    
+    def __repr__(self):
+        """Node class __repr__ method."""
+        return "Node({value})".format(value=self.value)
+      
+
+class SinglyLinkedList(object):
+    """SinglyLinkedList class implementation."""
+    
+    def __init__(self):
+        """Initialize SinglyLinkedList class object instance."""
+        self.head = None
+
+    def __str__(self):
+        """SinglyLinkedList class __str__ method."""
+        values = ""
+
+        if self.head:
+            node = self.head
+
+            while node:
+                values += str(node)
+
+                if node.next:
+                    values += ", "
+
+                node = node.next
+
+        return "[{values}]".format(values=values)
+
+    def __repr__(self):
+        """SinglyLinkedList class __repr__ method."""
+        values = ""
+
+        if self.head:
+            node = self.head
+
+            while node:
+                values += ".push_front({value})".format(
+                    value=repr(node.value)
+                )
+                node = node.next
+
+        return "SinglyLinkedList(){values}".format(values=values)
+
+    def push_front(self, value):
+        """Insert item at the head of the list."""
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+        
+    def top_front(self):
+        """Return first item."""
+        if self.head:
+            return self.head.value
+    
+    def pop_front(self):
+        """Remove first item."""
+        if self.head:
+            node = self.head.next
+            del self.head
+            self.head = node
+
+    def push_back(self, value):
+        """Insert item at the end of the list."""
+        node = Node(value)
+        if self.head:
+            iter_node = self.head
+
+            while iter_node.next:
+                iter_node = iter_node.next
+        
+            iter_node.next = node
+        else:
+            self.head = node
+    
+    def top_back(self):
+        """Return last item."""
+        if self.head:
+            node = self.head
+
+            while node.next:
+                node = node.next
+
+            return node.value
+    
+    def pop_back(self):
+        """Remove last item."""
+        if self.head:
+            if not self.head.next:
+                del self.head
+                self.head = None
+            else:
+                node = self.head
+
+                while node.next.next:
+                    node = node.next
+
+                del node.next
+                node.next = None
+
+    def find(self, value):
+        """Return if any item in the list matches provided value."""
+        if self.head:
+            node = self.head
+
+            while node:
+                if node.value == value:
+                    return True
+
+                node = node.next
+
+        return False
+    
+    def erase(self, value):
+        """Remove item with from the list that matches provided value."""
+        if self.head:
+            node = self.head
+            prev = None
+
+            while node:
+                if node.value == value:
+                    if prev:
+                        prev.next = node.next
+                    else:
+                        self.head = node.next
+                    del node
+                    break
+
+                prev = node
+                node = node.next
+
+    def length(self):
+        """Return length of the list."""
+        if self.head:
+            node = self.head
+            count = 0
+
+            while node:
+                count += 1
+                node = node.next
+
+            return count
+
+        return 0
+
+    def empty(self):
+        """Return if the list is empty."""
+        return not self.head
---
+++ 2.Data_Structures/2.Linked_List/Python/test_singly.py
@@ -0,0 +1,72 @@
+from singly import SinglyLinkedList
+
+
+def test_create_read_delete():
+    sll = SinglyLinkedList()
+    sll.push_front(1)
+    sll.push_front(2)
+    assert sll.length() == 2
+    assert sll.top_front() == 2
+    assert sll.top_back() == 1
+    sll.pop_front()
+    assert sll.top_front() == 1
+    assert sll.top_front() == sll.top_back()
+    assert sll.length() == 1
+    sll.push_back(2)
+    assert sll.length() == 2
+    assert sll.top_front() == 1
+    assert sll.top_back() == 2
+    sll.pop_back()
+    assert sll.top_front() == 1
+    assert sll.top_front() == sll.top_back()
+    assert sll.length() == 1
+
+
+def test_length():
+    sll = SinglyLinkedList()
+    for x in range(50):
+        sll.push_back(x)
+
+    assert sll.length() == 50
+    assert sll.top_front() == 0
+    assert sll.top_back() == 49
+
+    for x in range(21):
+        sll.push_front(x)
+
+    assert sll.length() == 71
+    assert sll.top_front() == 20
+    assert sll.top_back() == 49
+
+
+def test_find():
+    sll = SinglyLinkedList()
+    for x in range(50):
+        sll.push_back(x)
+
+    assert sll.find(0)
+    assert sll.find(13)
+    assert sll.find(49)
+    assert not sll.find(50)
+    assert not sll.find(-1)
+
+
+def test_erase():
+    sll = SinglyLinkedList()
+    for x in range(50):
+        sll.push_back(x)
+
+    sll.erase(49)
+    sll.erase(0)
+    sll.erase(13)
+    assert sll.top_back() == 48
+    assert sll.top_front() == 1
+    assert not sll.find(13)
+    assert sll.length() == 47
+
+
+def test_empty():
+    sll = SinglyLinkedList()
+    assert sll.empty()
+    sll.push_front(1)
+    assert not sll.empty()
