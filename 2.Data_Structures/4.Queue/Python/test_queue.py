07a0f339d2a2e8dffedcc6d5cbc2e3031106e5aa
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-10-12 19:47:32 +0200

    Implement queue tests

---
+++ 2.Data_Structures/4.Queue/Python/test_queue.py
@@ -0,0 +1,31 @@
+import pytest
+from queue_array import ArrayQueue
+from queue_linked_list import LinkedListQueue
+
+
+@pytest.mark.parametrize("queue", [
+    ArrayQueue(3),
+    LinkedListQueue(),
+])
+def test_queue(queue):
+    assert queue.empty()
+    with pytest.raises(IndexError):
+        queue.dequeue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+
+    if isinstance(queue, ArrayQueue):
+        with pytest.raises(IndexError):
+            queue.enqueue(4)
+
+    assert queue.dequeue() == 1
+    assert queue.dequeue() == 2
+    assert not queue.empty()
+    assert queue.dequeue() == 3
+    assert queue.empty()
+
+    obj = eval(repr(queue))
+    queue_type = type(queue)
+    assert isinstance(queue, queue_type) == isinstance(obj, queue_type)
+    assert queue.__dict__ == obj.__dict__
