7191c7a2345629a235a406950e37940dc555257b
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-10-17 19:09:01 +0200

    Add docstring

--- src/dsa/algorithms/heap_sort.py
+++ src/dsa/algorithms/heap_sort.py
@@ -1,7 +1,22 @@
+"""
+https://en.wikipedia.org/wiki/Heapsort
+https://www.cs.usfca.edu/~galles/visualization/HeapSort.html
+"""
 from dsa.tree.heap import MaxHeap
 
 
 def heap_sort(array):
+    """Heap sort algorithm implementation.
+
+    Time complexity: O(n*log(n))
+    Space complexity: O(n)
+
+    Args:
+        array(List[int]): Array of integers to sort
+
+    Returns:
+        Sorted array
+    """
     sorted = []
     heap = MaxHeap(array)
 
