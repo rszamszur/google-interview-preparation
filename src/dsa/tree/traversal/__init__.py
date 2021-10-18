360f70e867e7b4aba56ef2c1e68074c196bec81a
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-10-18 19:53:08 +0200

    Configure imports for traversal submodule

---
+++ src/dsa/tree/traversal/__init__.py
@@ -0,0 +1,28 @@
+from dsa.tree.traversal.breadth_first import (
+    BreadthFirstBinaryTree,
+    BreadthFirstGeneralTree,
+)
+from dsa.tree.traversal.inorder import (
+    InorderBinaryTree,
+    InorderGeneralTree,
+)
+from dsa.tree.traversal.preorder import (
+    PreorderGeneralTree,
+    PreorderBinaryTree,
+)
+from dsa.tree.traversal.postorder import (
+    PostorderBinaryTree,
+    PostorderGeneralTree,
+)
+
+
+__all__ = (
+    BreadthFirstBinaryTree,
+    BreadthFirstGeneralTree,
+    InorderBinaryTree,
+    InorderGeneralTree,
+    PreorderBinaryTree,
+    PreorderGeneralTree,
+    PostorderBinaryTree,
+    PostorderGeneralTree,
+)
