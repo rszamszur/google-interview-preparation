2abb1aff0018573b6f541797cc82c3da9f1c8434
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-10-25 20:29:57 +0200

    Add minor docstring change

--- src/dsa/graph/traversal/bfs.py
+++ src/dsa/graph/traversal/bfs.py
@@ -3,22 +3,22 @@ For a graph input one can provide either Graph object or dictionary of
 lists/sets.
 
 Example usage:
-    from dsa.graph import Graph
+from dsa.graph import Graph
 
 
-    graph = Graph(directed=True)
-    graph.add_edge("a", "z")
-    graph.add_edge("a", "s")
-    graph.add_edge("s", "x")
-    graph.add_edge("x", "d")
-    graph.add_edge("x", "c")
-    graph.add_edge("d", "c")
-    graph.add_edge("d", "f")
-    graph.add_edge("c", "f")
-    graph.add_edge("c", "v")
-    graph.add_edge("f", "v")
+graph = Graph(directed=True)
+graph.add_edge("a", "z")
+graph.add_edge("a", "s")
+graph.add_edge("s", "x")
+graph.add_edge("x", "d")
+graph.add_edge("x", "c")
+graph.add_edge("d", "c")
+graph.add_edge("d", "f")
+graph.add_edge("c", "f")
+graph.add_edge("c", "v")
+graph.add_edge("f", "v")
 
-    bfs = bfs(graph, "a")
+bfs = bfs(graph, "a")
 
 """
 from collections import deque
@@ -32,7 +32,7 @@ def bfs(graph, start):
 
     Args:
         graph(dict or Graph): Dictionary of set values.
-        start(any): Start vertex.
+        start(Any): Start vertex.
 
     Returns:
         Breadth first search nodes set.
