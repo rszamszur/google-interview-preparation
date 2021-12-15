04b8b88d471f934b6180407a277e381d499251c7
Radosław Szamszur <radoslawszamszur@gmail.com>
2021-12-15 19:46:22 +0100

    Extend unit test cases

--- src/test/problems/test_binary_tree_inorder_traversal.py
+++ src/test/problems/test_binary_tree_inorder_traversal.py
@@ -9,6 +9,7 @@ from .utils import recreate_binary_tree
 @pytest.mark.parametrize("tree_data, expected", [
     ([1, None, 2, 3], [1, 3, 2]),
     ([1], [1]),
+    ([1, 2, 3, 4, 5, None, None], [4, 2, 5, 1, 3])
 ])
 def test_inorder_traversal(tree_data, expected):
     root = recreate_binary_tree(tree_data, TreeNode)
