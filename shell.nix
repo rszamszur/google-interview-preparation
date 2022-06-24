0e101b2b566a467653c40b8db6dfb4ddbb53e673
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-06-24 18:40:54 +0200

    Update .gitignore, add .envrc, add shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python39
+    (pkgs.poetry.override { python = pkgs.python39; })
+  ];
+  shellHook = ''
+    export POETRY_HOME=${pkgs.poetry}
+    export POETRY_VIRTUALENVS_IN_PROJECT=true
+    unset SOURCE_DATE_EPOCH
+  '';
+}
