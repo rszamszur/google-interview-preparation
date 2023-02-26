b209da627b653d15f4c43038b4869976397a5d39
Radosław Szamszur <radoslawszamszur@gmail.com>
2023-02-26 12:52:16 +0100

    Add util for asserting trees equality

--- src/test/problems/utils.py
+++ src/test/problems/utils.py
@@ -1,6 +1,21 @@
 from collections import deque
 
 
+def assert_trees_equal(tree, tree2):
+    queue = deque()
+    queue.append((tree, tree2))
+
+    while queue:
+        a, b = queue.popleft()
+        assert a.val == b.val
+
+        if a.left:
+            queue.append((a.left, b.left))
+
+        if a.right:
+            queue.append((a.right, b.right))
+
+
 def recreate_binary_tree(array_data, node_class):
     root = node_class(val=array_data.pop(0))
     queue = deque()
